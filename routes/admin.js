ead49f3e84f7b47ef4172dcf4bb87da09bab142d
klaus01 <kelei0017@gmail.com>
2015-02-12 18:15:39 +0800

    增加后台用户管理功能

--- routes/admin.js
+++ routes/admin.js
@@ -12,51 +12,64 @@ function resRedirect(res, path) {
 	res.redirect('/' + PATHHEADER + path);
 }
 
-
-router.get('/', function(req, res, next) {
+function checkIsLogged(req, res, next) {
     if (req.session.user)
+        next();
+    else
+        resRedirect(res, '/login');
+}
+
+function checkIsManager(req, res, next) {
+    if (req.session.user.IsManager)
+        next();
+    else
+        resRedirect(res, '/');
+}
+
+router.get('/', function(req, res) {
+    checkIsLogged(req, res, function() {
         resRender(res, 'index', {
             title: '管理首页',
             user: req.session.user
         });
-    else
-        resRedirect(res, '/login');
+    });
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', function(req, res) {
 	resRender(res, 'login', { title: '用户登录' });
 });
 
-router.post('/login', function(req, res, next) {
+router.post('/login', function(req, res) {
 
-    function loginError(message) {
+    function error(message) {
         resRender(res, 'login', {
             title: '用户登录',
             message: message
         });
     }
 
-    if (!req.body.username) {
-        loginError('请输入用户名');
+    var data = req.body;
+    if (!data.username) {
+        error('请输入用户名');
         return;
     }
-    if (!req.body.password) {
-        loginError('请输入密码');
+    if (!data.password) {
+        error('请输入密码');
         return;
     }
-    if (!req.body.verificationCode) {
-        loginError('请输入验证码');
+    if (!data.verificationCode) {
+        error('请输入验证码');
         return;
     }
     var verificationCode = req.session.verificationCode;
-    if (!verificationCode || verificationCode !== req.body.verificationCode.toUpperCase()) {
-        loginError('验证码错误');
+    if (!verificationCode || verificationCode !== data.verificationCode.toUpperCase()) {
+        error('验证码错误');
         return;
     }
 
-	dbHelper.backendUsers.find(req.body.username, function(rows) {
+	dbHelper.backendUsers.find(data.username, function(rows) {
 		if (rows.length) {
-            if (req.body.password === rows[0].LoginPassword) {
+            if (data.password === rows[0].LoginPassword) {
                 req.session.user = rows[0];
                 dbHelper.backendUsers.updateLoginInfo(req.session.user.ID, req.connection.remoteAddress);
                 resRedirect(res, '/');
@@ -67,23 +80,80 @@ router.post('/login', function(req, res, next) {
 	});
 });
 
-router.get('/logout', function(req, res, next) {
+router.get('/logout', function(req, res) {
 	req.session.user = null;
 	resRedirect(res, '/');
 });
 
-router.get('/backendUsers', function(req, res, next) {
-    if (req.session.user) {
-        if (req.session.user.IsManager)
-            resRender(res, 'backendUsers', {
-                title: '后台用户管理',
-                user: req.session.user
+router.get('/backendUsers', function(req, res) {
+    checkIsLogged(req, res, function() {
+        checkIsManager(req, res, function() {
+            dbHelper.backendUsers.find(function(rows) {
+                resRender(res, 'backendUsers', {
+                    title: '后台用户管理',
+                    user: req.session.user,
+                    rows: rows
+                });
+            })
+        });
+    });
+});
+
+router.get('/newBackendUser', function(req, res) {
+    checkIsLogged(req, res, function() {
+        checkIsManager(req, res, function() {
+            resRender(res, 'newBackendUser', {
+                title: '新增后台用户',
+                id: 0
             });
-        else
-            resRedirect(res, '/');
-    }
-    else
-        resRedirect(res, '/login');
+        });
+    });
+});
+
+router.post('/newBackendUser', function(req, res) {
+    checkIsLogged(req, res, function() {
+        checkIsManager(req, res, function() {
+
+            function showMessage(id, isError, message) {
+                resRender(res, 'newBackendUser', {
+                    title: '新增后台用户',
+                    id: id,
+                    isError: isError,
+                    message: message
+                });
+            }
+            function error(message) {
+                showMessage(req.body.id, true, message);
+            }
+            function success(rows) {
+                console.log(rows);
+                showMessage(0, false, '成功');
+            }
+
+            var data = req.body;
+            if (!data.name) {
+                error('请输入姓名');
+                return;
+            }
+            if (!data.loginname) {
+                error('请输入登录名');
+                return;
+            }
+            if (!data.password) {
+                error('请输入密码');
+                return;
+            }
+            dbHelper.backendUsers.find(data.loginname, data.id, function(rows) {
+                if (rows.length > 0)
+                    error('登录名' + data.loginname + '已存在，请更换');
+                else if (data.id > 0)
+                    dbHelper.backendUsers.update(data.id, data.name, 0, data.loginname, data.password, success);
+                else
+                    dbHelper.backendUsers.new(data.name, 0, data.loginname, data.password, success);
+            });
+
+        });
+    });
 });
 
 

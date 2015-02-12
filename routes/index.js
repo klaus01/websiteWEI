c4a82578e0f4174ba254ba8873d934d52431c77e
kelei <kelei0017@gmail.com>
2015-02-12 10:44:54 +0800

    首字母小写

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 
-router.get('/VerificationCode', function(req, res, next) {
+router.get('/verificationCode', function(req, res, next) {
     var ary = ccap.get();
     req.session.verificationCode = ary[0];
     res.end(ary[1]);

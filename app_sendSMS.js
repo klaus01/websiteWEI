564069d68a81fb9d7af7e260d04467f067dc861b
klaus01 <kelei0017@gmail.com>
2015-03-11 21:28:39 +0800

    修改文字

--- app_sendSMS.js
+++ app_sendSMS.js
@@ -1,17 +1,20 @@
+/**
+ * 后台发送短信单元
+ */
+
 var dbConn = require('./lib/dbConn');
 var dbHelper = require('./lib/dbHelper');
-var publicFunction = require('./lib/publicFunction');
 var settings = require('./settings');
 
 function timer(){
     console.log(new Date(), '开始查询待发送短信列表');
-    dbHelper.messages.findWaiting(function(rows){
-        console.log(new Date(), '查询待发送短信列表成功，有' + rows.length + '条');
+    dbHelper.messages.findWaitingSMS(function(rows){
+        console.log(new Date(), '查询完成，有' + rows.length + '条');
         for (var i = 0; i < rows.length; i++) {
             var row = rows[i];
-            console.log(new Date(), row.ID, '发送', '成功', row.PhoneNumber, row.Contnet);
+            console.log(new Date(), row.ID, '发送', '成功', row.PhoneNumber, row.Content);
             // TODO 调用短信接口发送短信
-            dbHelper.messages.updateAsSent(row.ID, function(){
+            dbHelper.messages.updateSMSAsSent(row.ID, function(){
                 console.log(new Date(), row.ID, '标记为已发送', '成功');
             });
         }

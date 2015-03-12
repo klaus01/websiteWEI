a7859ba78ede5fa9b713eddd11cb63f07c6f8c5f
klaus01 <kelei0017@gmail.com>
2015-03-12 16:59:50 +0800

    修改错误

--- routes/ajax.js
+++ routes/ajax.js
@@ -24,7 +24,7 @@ function success(res, data) {
 }
 
 function checkIsManager(req, res, isManager) {
-    if (req.session.adminUser.IsManager)
+    if (req.session.adminUser && req.session.adminUser.IsManager)
         isManager();
     else
         error(res, '不是管理员用户');
@@ -55,20 +55,20 @@ router.post('/backendUser/post', function(req, res) {
     checkIsManager(req, res,
         function() {
             var data = req.body;
-            if (!data.id) {
+            if (data.id == undefined || parseInt(data.id) == undefined) {
                 error(res, '缺少id');
                 return;
             }
             var id = parseInt(data.id);
-            if (!data.name) {
+            if (data.name == undefined || !data.name.length) {
                 error(res, '缺少姓名');
                 return;
             }
-            if (!data.loginname) {
+            if (data.loginname == undefined || !data.loginname.length) {
                 error(res, '缺少登录名');
                 return;
             }
-            if ((id <= 0) && !data.password) {
+            if ((id <= 0) && (data.password == undefined || !data.password.length)) {
                 error(res, '缺少密码');
                 return;
             }
@@ -77,12 +77,12 @@ router.post('/backendUser/post', function(req, res) {
                 if (rows.length > 0)
                     error(res, '登录名' + data.loginname + '已存在，请更换');
                 else if (id > 0)
-                    dbHelper.backendUsers.update(id, data.name, 0, data.loginname, data.password, function (data) {
-                        success(res, data);
+                    dbHelper.backendUsers.update(id, data.name, 0, data.loginname, data.password, function (result) {
+                        success(res, null);
                     });
                 else
-                    dbHelper.backendUsers.new(data.name, 0, data.loginname, data.password, function (data) {
-                        success(res, data);
+                    dbHelper.backendUsers.new(data.name, 0, data.loginname, data.password, function (newID) {
+                        success(res, {newID: newID});
                     });
             });
         }
@@ -93,8 +93,8 @@ router.post('/backendUser/delete', function(req, res) {
     checkIsManager(req, res,
         function() {
             var id = req.body.id;
-            if (id)
-                dbHelper.backendUsers.delete(id, function(rows){
+            if (id != undefined && parseInt(id))
+                dbHelper.backendUsers.delete(id, function(){
                     success(res, null);
                 });
             else
@@ -109,7 +109,7 @@ router.post('/backendUser/delete', function(req, res) {
 
 router.post('/partnerUser/get', function(req, res) {
     var id = req.body.id;
-    if (id)
+    if (id != undefined && parseInt(id))
         dbHelper.partnerUsers.findByID(parseInt(id), function(rows){
             if (rows.length) {
                 var data = rows[0];
@@ -135,12 +135,12 @@ router.post('/partnerUser/get', function(req, res) {
 router.post('/partnerUser/post', function(req, res) {
     var data = req.body;
     var files = req.files;
-    if (!data.id) {
+    if (data.id == undefined || parseInt(data.id) == undefined) {
         error(res, '缺少id');
         return;
     }
     var id = parseInt(data.id);
-    if (!data.name) {
+    if (data.name == undefined || !data.name.length) {
         error(res, '缺少名称');
         return;
     }
@@ -148,19 +148,19 @@ router.post('/partnerUser/post', function(req, res) {
         error(res, '缺少头像');
         return;
     }
-    if (!data.description) {
+    if (data.description == undefined || !data.description.length) {
         error(res, '缺少描述');
         return;
     }
-    if (!data.loginName) {
+    if (data.loginName == undefined || !data.loginName.length) {
         error(res, '缺少登录名');
         return;
     }
-    if ((id <= 0) && !data.password) {
+    if ((id <= 0) && (data.password == undefined || !data.password.length)) {
         error(res, '缺少密码');
         return;
     }
-    if (!data.enabled) {
+    if (data.enabled == undefined || !data.enabled.length) {
         error(res, '缺少启用状态');
         return;
     }
@@ -176,12 +176,12 @@ router.post('/partnerUser/post', function(req, res) {
         function operatingDB(){
             var iconFileName = files.iconFile ? files.iconFile.name : null;
             if (id > 0)
-                dbHelper.partnerUsers.update(id, data.name, iconFileName, data.description, data.loginName, data.password, data.enabled, appUserID, function (data) {
-                    success(res, data);
+                dbHelper.partnerUsers.update(id, data.name, iconFileName, data.description, data.loginName, data.password, data.enabled, appUserID, function (result) {
+                    success(res, null);
                 });
             else
-                dbHelper.partnerUsers.new(data.name, iconFileName, data.description, data.loginName, data.password, data.enabled, appUserID, function (data) {
-                    success(res, data);
+                dbHelper.partnerUsers.new(data.name, iconFileName, data.description, data.loginName, data.password, data.enabled, appUserID, function (newID) {
+                    success(res, {newID: newID});
                 });
         }
 
@@ -215,7 +215,7 @@ router.post('/activity/post', function(req, res) {
     var data = req.body;
     var files = req.files;
 
-    if (!data.partnerUserID) {
+    if (data.partnerUserID == undefined || parseInt(data.partnerUserID) == undefined) {
         error(res, '缺少公众号ID');
         return;
     }
@@ -223,36 +223,59 @@ router.post('/activity/post', function(req, res) {
         error(res, '公众号ID错误');
         return;
     }
-    if (!data.mode) {
+    if (data.mode == undefined || parseInt(data.mode) == undefined) {
         error(res, '缺少活动类型');
         return;
     }
     mode = parseInt(data.mode);
     switch(mode) {
         case 2:
-            if (!data.longitude) {
+            if (data.longitude == undefined || parseFloat(data.longitude) == undefined) {
                 error(res, '缺少经度');
                 return;
             }
-            if (!data.latitude) {
+            if (data.latitude == undefined || parseFloat(data.latitude) == undefined) {
                 error(res, '缺少纬度');
                 return;
             }
-            if (!data.distanceMeters) {
+            if (!data.distanceMeters == undefined || parseInt(data.distanceMeters) == undefined || parseInt(data.distanceMeters) <= 0) {
                 error(res, '缺少距离');
                 return;
             }
         case 1:
-            if (!data.beginTime) {
+            if (data.beginTime == undefined || !data.beginTime.length) {
                 error(res, '缺少开始时间');
                 return;
             }
-            if (!data.endTime) {
+            if (data.endTime == undefined || !data.endTime.length) {
                 error(res, '缺少结束时间');
                 return;
             }
-            if (!data.expireAwardTime) {
+            if (data.expireAwardTime == undefined || !data.expireAwardTime.length) {
                 error(res, '缺少终止领奖时间');
+                return;
+            }
+            var beginTime = new Date(data.beginTime);
+            var endTime = new Date(data.endTime);
+            var expireAwardTime = new Date(data.expireAwardTime);
+            if (isNaN(beginTime.getTime())) {
+                error(res, '开始时间格式错误');
+                return;
+            }
+            if (isNaN(endTime.getTime())) {
+                error(res, '结束时间格式错误');
+                return;
+            }
+            if (isNaN(expireAwardTime.getTime())) {
+                error(res, '终止领奖时间格式错误');
+                return;
+            }
+            if (endTime < new Date()) {
+                error(res, '"结束时间"应在当前时间之后');
+                return;
+            }
+            if (beginTime > endTime || expireAwardTime < endTime) {
+                error(res, '"开始时间"应在"结束时间"之前，"结束时间"应在"终止领奖时间"之前');
                 return;
             }
         default:
@@ -260,15 +283,15 @@ router.post('/activity/post', function(req, res) {
                 error(res, '缺少图片');
                 return;
             }
-            if (!data.content) {
+            if (data.content == undefined || !data.content.length) {
                 error(res, '缺少文字内容');
                 return;
             }
     }
 
     publicFunction.moveActivityPictureFile(data.partnerUserID, files.pictureFile, function(){
-        dbHelper.activities.new(data.partnerUserID, mode, files.pictureFile.name, data.content, data.url, data.beginTime, data.endTime, data.expireAwardTime, data.longitude, data.latitude, data.distanceMeters, function (data) {
-            var activityID = data.insertId;
+        dbHelper.activities.new(data.partnerUserID, mode, files.pictureFile.name, data.content, data.url, data.beginTime, data.endTime, data.expireAwardTime, data.longitude, data.latitude, data.distanceMeters, function (newID) {
+            var activityID = newID;
             dbHelper.appUsers.findFriendsByPartnerUserID(data.partnerUserID, function(rows){
                 var i = 0;
                 function next(){
@@ -277,7 +300,7 @@ router.post('/activity/post', function(req, res) {
                     dbHelper.messages.newActivityMessage(data.partnerUserID, user.AppUserID, activityID, user.APNSToken, data.content, next);
                 }
                 next();
-                success(res, data);
+                success(res, {newID: activityID});
             });
         });
     });
